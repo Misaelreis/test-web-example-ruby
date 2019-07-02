ec34c44f35fdb07322b3479eb96c30b9ccb19e7b
Misaelreis <zael.au@gmail.com>
2019-07-02 07:40:26 -0300

    update

---
+++ features/specs/interagindo_com_dropdown_select.feature
@@ -0,0 +1,10 @@
+#language: pt
+ 
+ @select
+ Funcionalidade: interagindo com dropdown e select
+
+ Cenário: Interagir com o dropdown e select
+ Quando eu interagir com o dropdown e select
+
+ Cenário: Preencher o autocomplete
+ Quando eu preencher o autocomplete
---
+++ features/step_definitions/interagindo_com_dropdown_select.rb
@@ -0,0 +1,17 @@
+Quando("eu interagir com o dropdown e select") do
+    visit('/buscaelementos/dropdowneselect')
+    find('.btn.dropdown-button').click
+    find('#dropdown3').click
+    #select
+    select 'Chrome', from: 'dropdown'
+    find('option[value="2"]').select_option
+    sleep 3
+    #autocomplete
+    visit('/widgets/autocomplete')
+    find('#autocomplete-input').set'Rio de Jane'
+    find('ul', text: 'Rio de Janeiro').click
+    sleep 3
+end
+  
+Quando("eu preencher o autocomplete") do
+end
